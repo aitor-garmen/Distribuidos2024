773dee62b5e1333464153e6af702d5d6cd37363a
aitor-garmen <99321692+aitor-garmen@users.noreply.github.com>
2024-02-19 17:31:18 +0100

    Update factorial.m

--- factorial.m
+++ factorial.m
@@ -1,16 +1,16 @@
 clear all
 clc
 
-num=input('Introduzca un número entero:);
+num=input('Introduzca un número entero: '); % Faltaba una comilla en la línea 4 cuando se solicita el input.
 
-factorial=1
+factorial_1=1; % He añadido el punto y coma, y le he cambiado el nombre a la variable factorial, y le he puesto factorial_1 porque tenia el mismo nombre que el archivo.
 
 for i=1:num-1
-    factorial=factorial*i
+    factorial_1=factorial_1*i;
 end
 
 if num>=0
-    disp('El factorial de' num2str(num) 'es:' factorial)
+    disp(['El factorial de ', num2str(num), ' es: ', num2str(factorial_1)]) % Faltaba el num2str, y el corchete
 else
-    disp(['El factorial de',num,'es:',factorial])
+    disp(['El factorial de ',num2str(num),' no se puede calcular al ser un numero negativo']) % No tiene sentido que fuese lo mismo cuando se cumple ca condicion y cuando no
 end
